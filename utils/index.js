83fef89bdc9fa0cc2c03b4c5a42151bb6de91b2c
Walter V. Faustine <waltervfaustine@gmail.com>
2019-08-22 11:15:52 +0300

    feat: 🎸 added support for chart type configuration
    This commit will add the chart type configuration i.e column or bar
    chart

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,24 @@
-const _90_90_90_CascadeGraph = function(config, context) {
-    
+/**
+ * 
+ * @param {string} config 
+ * @param {string} context 
+ * @param {string} ctype 
+ */
+const _90_90_90_CascadeGraph = function (config, context, ctype) { };
+
+/**
+ * @typedef {*} string
+ */
+
+/**
+ * 
+ * @param {string} ctype 
+ */
+const chartConfig = function (ctype) {
+    return {
+        type: ctype,
+    }
 }
 
-
 exports._90_90_90_CascadeGraph = _90_90_90_CascadeGraph;
+
