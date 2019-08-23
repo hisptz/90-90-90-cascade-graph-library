f1bc6362ba2564ea85ea1a0c6b47c7bf566d2b1c
Walter V. Faustine <waltervfaustine@gmail.com>
2019-08-23 12:16:46 +0300

    fix: 🐛 updated test for the index-spec.js
    This commit will update the test file

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,10 +1,10 @@
 var assert = require('assert');
-var _90_90_90_CascadeGraph = require('../src/app/index');
+var GenerateCascadeGraph = require('../src/app/index');
 
-describe('_90_90_90_CascadeGraph', function () {
-    describe('add _90_90_90_CascadeGraph', function () {
+describe('GenerateCascadeGraph', function () {
+    describe('add GenerateCascadeGraph', function () {
         it('return _90_90_90_CascadeChartObject', function () {
-            var result = _90_90_90_CascadeGraph._90_90_90_CascadeGraph(
+            var result = GenerateCascadeGraph.GenerateCascadeGraph(
                 'nacp',
                 'dhis2',
                 'column'
